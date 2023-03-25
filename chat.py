5b217111e3900be8d730419d209199eed29af0ef
Abhay Gupta <89296170+Abhayg444@users.noreply.github.com>
2023-03-25 17:47:47 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -59,7 +59,7 @@ def generate_response(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=25,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.7,
