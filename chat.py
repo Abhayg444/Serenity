e573c5db224eef9fa4363bfd6411d33332cb2653
Abhay Gupta <89296170+Abhayg444@users.noreply.github.com>
2023-03-27 16:39:24 +0000

    GPT engine updation

--- chat.py
+++ chat.py
@@ -57,7 +57,7 @@ def get_response(intents_list, intents_json):
 
 def generate_response(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=1024,
         n=1,
