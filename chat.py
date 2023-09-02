31964d84016ed5b65eb4dbe69d6c4968af73063c
Abhay Gupta <89296170+Abhayg444@users.noreply.github.com>
2023-09-02 21:41:08 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import load_model
 bot_name = 'Serenity'
-openai.api_key = "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" 
+openai.api_key = "enter your own"
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
